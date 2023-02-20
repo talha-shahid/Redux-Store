faec24fe69b0a9053107dc0f243bd54689de780d
Talha Shahid <talhashahidch@gmail.com>
2023-02-20 20:48:38 +0500

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const items = useSelector((state) => state.cart);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        background: "#764abc",
+        color: "white",
+        padding: "1.3rem 1rem",
+      }}
+    >
+      <span className="logo">REDUX STORE</span>
+      <div>
+        <Link className="navLink" to="/">
+          Home
+        </Link>
+        <Link className="navLink" to="/cart">
+          Cart
+        </Link>
+        <span className="cartCount">Cart items: {items.length}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Products.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { add } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../store/productSlice";
+import { STATUSES } from "../store/productSlice";
+
+const Products = () => {
+  const dispatch = useDispatch();
+  // const [products, setProducts] = useState([]);
+  const { data: products, status } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+    // const fetchProducts = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products");
+    //   const data = await res.json();
+    //   console.log(data);
+    //   setProducts(data);
+    // };
+    // fetchProducts();
+  }, []);
+
+  const handleAdd = (product) => {
+    dispatch(add(product));
+  };
+
+  if (status === STATUSES.LOADING) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (status === STATUSES.ERROR) {
+    return <h2>Something went wrong!</h2>;
+  }
+
+  return (
+    <div className="productsWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="" />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button onClick={() => handleAdd(product)} className="btn">
+            Add to cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
